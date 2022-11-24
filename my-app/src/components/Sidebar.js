e034264e759c2bfb882ada9b13e00bf2f7024749
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2022-11-24 10:57:32 +0000

    finish functional components challenge

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sidebar = () => {
+    return(
+        <div>
+        <a href="#" target="_blank">My Photos</a>
+        <a href="#" target="_blank">My Illustrations</a>
+        <a href="#" target="_blank">My paintings</a>
+    </div>
+    )
+};
+
+export default Sidebar;
